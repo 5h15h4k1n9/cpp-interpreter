81f005b6015f1d1ed2f7f0c77435e894d901da10
5h15h4k1n9 <st077017@studetn.spbu.ru>
2022-11-22 12:38:13 +0300

    Added converting to signed/unsigned int and to bool value

--- src/interpreter/TypeOperations.hs
+++ src/interpreter/TypeOperations.hs
@@ -1,18 +1,29 @@
-module TypeOperations(module TypeOperations) where
+module TypeOperations where
 
 import Ast
 import Errors
 
-toInt :: CppValue -> CppValue
-toInt  VVoid       = error $ cannotConvertException TVoid (TInt Signed)
-toInt (VInt   _ _) = undefined
-toInt (VBool  v  ) = VInt (if v then 1 else 0)     Signed
-toInt (VChar  v  ) = VInt (toInteger $ fromEnum v) Signed
-toInt (VArray _ t) = error $ cannotConvertException (TArray t 0) (TInt Signed)
+toSInt :: CppValue -> CppValue
+toSInt VVoid          = error $ cannotConvertException TVoid (TInt Signed)
+toSInt (VInt   v _  ) = VInt nv                       Signed where
+    maxSInt = 2147483648
+    vDiv = v `div` maxSInt
+    vMod = v `mod` maxSInt
+    nv = vMod - (vDiv `mod` 2) * maxSInt
+toSInt (VBool  v    ) = VInt (if v then 1 else 0)     Signed
+toSInt (VChar  v    ) = VInt (toInteger $ fromEnum v) Signed
+toSInt (VArray _ _ t) = error $ cannotConvertException (TArray t) (TInt Signed)
+
+toUInt :: CppValue -> CppValue
+toUInt VVoid          = error $ cannotConvertException TVoid (TInt Unsigned)
+toUInt (VInt   v _  ) = VInt (v `mod` 4294967296)     Unsigned
+toUInt (VBool  v    ) = VInt (if v then 1 else 0)     Unsigned
+toUInt (VChar  v    ) = VInt (toInteger $ fromEnum v) Unsigned
+toUInt (VArray _ _ t) = error $ cannotConvertException (TArray t) (TInt Unsigned)
 
 toBool :: CppValue -> CppValue
-toBool  VVoid       = error $ cannotConvertException TVoid TBool
-toBool (VInt   v _) = VBool (v /= 0)
-toBool (VBool  v  ) = VBool v
-toBool (VChar  v  ) = VBool (v /= '\0')
-toBool (VArray _ _) = error $ cannotConvertException (TArray TVoid 0) TBool
+toBool VVoid          = error $ cannotConvertException TVoid TBool
+toBool (VInt   v _  ) = VBool (v /= 0)
+toBool (VBool  v    ) = VBool v
+toBool (VChar  v    ) = VBool (v /= '\0')
+toBool (VArray _ _ t) = error $ cannotConvertException (TArray t) TBool
