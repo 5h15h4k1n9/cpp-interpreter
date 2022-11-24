5bb6b825535c1ce489ac9016b6163f7984d2beb4
5h15h4k1n9 <st077017@studetn.spbu.ru>
2022-11-24 22:06:11 +0300

    Cosmetic fixes

--- src/interpreter/TypeCheck.hs
+++ src/interpreter/TypeCheck.hs
@@ -3,14 +3,14 @@ module TypeCheck where
 import Ast
 
 getType :: CppValue -> CppType
-getType  VVoid                  = TVoid
-getType (VInt   _     modifier) = 
-  case modifier of
-    Signed                     -> TInt Signed
-    Unsigned                   -> TInt Unsigned
-getType (VBool  _             ) = TBool
-getType (VChar  _             ) = TChar
-getType (VArray _ _ t         ) = TArray t 
+getType  VVoid         = TVoid
+getType (VInt   _ m) = 
+  case m of
+    Signed            -> TInt Signed
+    Unsigned          -> TInt Unsigned
+getType (VBool  _    ) = TBool
+getType (VChar  _    ) = TChar
+getType (VArray _ _ t) = TArray t 
 
 checkType :: CppValue -> CppType -> Bool
 checkType v t = getType v == t
