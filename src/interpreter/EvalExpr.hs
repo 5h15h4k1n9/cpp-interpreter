4e927b56dfd0aed010bb87cd90eb8beead2a2604
5h15h4k1n9 <st077017@studetn.spbu.ru>
2022-11-26 10:13:04 +0300

    Refactored subtracting method

--- src/interpreter/EvalExpr.hs
+++ src/interpreter/EvalExpr.hs
@@ -5,6 +5,10 @@ import ValueOperations
 import Errors
 
 evalBinOp :: BinOp -> CppValue -> CppValue -> CppValue
+evalBinOp op  v1@VArray {} v2           = error $ cannotEvalBinOpException op v1 v2
+evalBinOp op  v1           v2@VArray {} = error $ cannotEvalBinOpException op v1 v2
+evalBinOp op  v1@VVoid     v2           = error $ cannotEvalBinOpException op v1 v2
+evalBinOp op  v1           v2@VVoid     = error $ cannotEvalBinOpException op v1 v2
 -- Arithmetic
 evalBinOp Add v1 v2 = v1   +   v2
 evalBinOp Sub v1 v2 = v1   -   v2
@@ -24,7 +28,7 @@ evalBinOp Or  v1 v2 = v1 `cppOr` v2
 
 evalUnOp :: UnOp -> CppValue -> CppValue
 evalUnOp op  v@VArray {} = error $ cannotEvalUnOpException op v
+evalUnOp op  v@VVoid     = error $ cannotEvalUnOpException op v
 evalUnOp Neg v           = -v
 evalUnOp Not (VBool b  ) = VBool (not b)
 evalUnOp Not v           = evalUnOp Not (toBool v)
-  
